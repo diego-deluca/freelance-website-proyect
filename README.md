fd28a1ff39c5c9dfe5ee36c94d9fc2659abd96ff
Diego <111003445+diego-deluca@users.noreply.github.com>
2022-09-11 11:16:15 -0300

    version 1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Freelance-web-proyect1
+Responsive HTML, CSS &amp; JS Freelance Website Proyect
+
+Sex Shop Website design in process. 
+
+Dark mode & sound implemented
+
---
+++ js/index.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".bx-right-arrow",
+    prevEl: ".bx-left-arrow",
+  },
+});
+const menu = document.querySelector(".menu");
+const checkBtn = document.querySelector("#check");
+const hambIcon = document.querySelector(".fa-bars");
+checkBtn.addEventListener("click", () => {
+  console.log(checkBtn.checked);
+  if (checkBtn.checked === true) {
+    menu.style.left = "0";
+    hambIcon.className = "fa-sharp fa-solid fa-xmark";
+  } else {
+    menu.style.left = "-100%";
+    hambIcon.className = "fa-solid fa-bars";
+  }
+});
